52abdbbf6cfc08f11c0cfc20892606fc76a963f3
synoga <synoga@gmail.com>
2013-09-05 16:48:24 +0200

    Update install--no-gui.sh

--- install--no-gui.sh
+++ install--no-gui.sh
@@ -15,6 +15,11 @@ fr_FR.UTF-8@euro UTF-8
 en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 
+if [ $? != 0 ]; then
+exit 2
+fi
+
+
 #On demande un login et un pass pour la SeedBox
 user=$1
 htpassword=$2
@@ -22,9 +27,17 @@ domain=$3
 
 #On peut donc mettre à jour le système et installer les paquets nécessaires.
 apt-get update -y && apt-get upgrade -y
+if [ $? != 0 ]; then
+exit 3
+fi
 apt-get install -y locales apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl libapache2-mod-scgi build-essential make gcc autoconf curl libcurl3 libcurl4-openssl-dev zip unzip libc6-dev linux-libc-dev diffutils wget bzip2 screen ffmpeg libcppunit-dev libncurses5-dev libncursesw5-dev subversion libsigc++-1.2-5c2 libsigc++-dev libsigc++-1.2-5 libsigc++-1.9-0 libsigc++-2.0-0c2a libsigc++-1.1-dev libsigc++-2.0-0 libsigc++0c2 libsigc++-2.0-dev libsigc++0 libsigc++0c102 libsigc++5 libsigc++-2.0-doc libsigc++-1.2-dev libsigc++-1.2-5c102 libsigc++-1.9-dev libsigc++-2.0-0c2 imagemagick zsh git openssl unrar-free mp3info libcurl4-openssl-dev mysql-server smbclient
+if [ $? != 0 ]; then
+exit 4
+fi
 apt-get autoremove -y
-
+if [ $? != 0 ]; then
+exit 5
+fi
 
 echo "###########################
 #                         #
@@ -41,36 +54,65 @@ cd sources
 
 #On récupère tout
 svn co https://svn.code.sf.net/p/xmlrpc-c/code/advanced xmlrpc-c
+if [ $? != 0 ]; then
+exit 6
+fi
 wget http://libtorrent.rakshasa.no/downloads/libtorrent-0.13.2.tar.gz
+if [ $? != 0 ]; then
+exit 7
+fi
 wget http://libtorrent.rakshasa.no/downloads/rtorrent-0.9.2.tar.gz
-
+if [ $? != 0 ]; then
+exit 8
+fi
 #On extrait !
 tar xvzf libtorrent-0.13.2.tar.gz
+if [ $? != 0 ]; then
+exit 9
+fi
 tar xvzf rtorrent-0.9.2.tar.gz
+if [ $? != 0 ]; then
+exit 10
+fi
 rm *.tar.gz
 
 #XMLRPC
 cd xmlrpc-c/
 ./configure
 make && make install
+if [ $? != 0 ]; then
+exit 11
+fi
 
 #libtorrent
 cd ../libtorrent-0.13.2/
 ./configure
 make && make install
+if [ $? != 0 ]; then
+exit 12
+fi
 
 #rtorrent
 cd ../rtorrent-0.9.2/
 ./autogen.sh 
 ./configure --with-xmlrpc-c
 make && make install
+if [ $? != 0 ]; then
+exit 13
+fi
 
 #On nettoie
 cd
 rm -Rf sources
+if [ $? != 0 ]; then
+exit 14
+fi
 
 #Y'a parfois une petite erreur avec la librairie 
 ldconfig
+if [ $? != 0 ]; then
+exit 15
+fi
 
 echo "#############################
 #                           #
@@ -80,6 +122,9 @@ echo "#############################
 
 # Création du groupe et de l'utilisateur qui executera rtorrent
 echo "$user:$htpassword:4242:4242:$user,,,:/home/$user:/bin/bash" | newusers
+if [ $? != 0 ]; then
+exit 16
+fi
 
 # Création des répertoires de fonctionnement de rtorrent
 # on créé un lien symbolique vers le répertoire de téléchargement dans la racine www
@@ -110,9 +155,13 @@ scgi_port = 127.0.0.1:5000
 ip = 127.0.0.1
 encryption = allow_incoming,require_RC4
 " > /home/$user/.rtorrent.rc
+if [ $? != 0 ]; then
+exit 17
+fi
 
 # On s'assure que rtorrent ne tourne pas
 killall rtorrent 2> /dev/null
+
 echo "###############################################
 #                                             #
 # Installation de RuTorrent et de ses plugins #
@@ -121,9 +170,15 @@ echo "###############################################
 
 cd /var/www
 svn checkout http://rutorrent.googlecode.com/svn/trunk/rutorrent/
+if [ $? != 0 ]; then
+exit 18
+fi
 cd rutorrent
 rm -R plugins
 svn checkout http://rutorrent.googlecode.com/svn/trunk/plugins/
+if [ $? != 0 ]; then
+exit 19
+fi
 chown -R www-data:www-data /var/www
 
 echo "##########################
@@ -136,6 +191,9 @@ echo "##########################
 service apache2 stop
 #Génération du .htpasswd que nous placons dans le dossier etc/apache2, à l'abri.
 htpasswd -mbc /etc/apache2/.htpasswd $user $htpassword
+if [ $? != 0 ]; then
+exit 20
+fi
 #Génération des clés de chiffrement
 cd /etc/ssl/certs/
 
@@ -150,16 +208,28 @@ echo "
 
 "
 openssl genrsa -des3 -out ca.key -passout file:.passwd 4096 
+if [ $? != 0 ]; then
+exit 21
+fi
 openssl req -passin file:.passwd -new -x509 -days 3650 -key ca.key -out ca.crt \
 -subj "/C=FR/ST=IDF/L=PARIS/O=42/OU=PROD/CN=$domain"
+if [ $? != 0 ]; then
+exit 22
+fi
 
 echo "
 #Génération d'une clé serveur et demande de signature.
 
 "
 openssl genrsa -passout file:.passwd -des3 -out server.key 4096
+if [ $? != 0 ]; then
+exit 23
+fi
 openssl req -new -key server.key -out server.csr -passin file:.passwd \
 -subj "/C=FR/ST=IDF/L=PARIS/O=42/OU=PROD/CN=$domain"
+if [ $? != 0 ]; then
+exit 24
+fi
 
 echo "
 #Signature du certificat avec l'autorité créée précédemment.
@@ -167,12 +237,17 @@ echo "
 "
 openssl x509 -passin file:.passwd -req -days 3650 -in server.csr \
 -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
-
+if [ $? != 0 ]; then
+exit 25
+fi
 echo "
 #Faire un fichier server.key qui n'implique pas une demande de mot de passe d'Apache.
 
 "
 openssl rsa -passin file:.passwd -in server.key -out server.key.insecure
+if [ $? != 0 ]; then
+exit 26
+fi
 
 echo "
 #Échange des clés.
@@ -280,13 +355,31 @@ ServerName http://$IP/
   </VirtualHost>
   </IfModule>
 DirectoryIndex index.html index.php /_h5ai/server/php/index.php' > /etc/apache2/conf.d/$user
+if [ $? != 0 ]; then
+exit 27
+fi
 
 #Activation des différents modules Apache
 a2enmod rewrite
+if [ $? != 0 ]; then
+exit 28
+fi
 a2enmod headers
+if [ $? != 0 ]; then
+exit 29
+fi
 a2enmod ssl
+if [ $? != 0 ]; then
+exit 30
+fi
 a2enmod auth_digest
+if [ $? != 0 ]; then
+exit 31
+fi
 a2enmod scgi
+if [ $? != 0 ]; then
+exit 32
+fi
 
 echo "#########################
 #                       #
@@ -296,6 +389,9 @@ echo "#########################
 
 cd /var/www/
 wget http://release.larsjung.de/h5ai/h5ai-0.22.1.zip
+if [ $? != 0 ]; then
+exit 33
+fi
 unzip h5ai-0.22.1.zip
 rm h5ai-0.22.1.zip 
 rm index.html 
@@ -312,12 +408,22 @@ cd mediainfo
 
 # On récupère !
 wget http://mediaarea.net/download/binary/libzen0/0.4.28/libzen0_0.4.28-1_amd64.Debian_6.0.deb
+if [ $? != 0 ]; then
+exit 33
+fi
 wget http://mediaarea.net/download/binary/libmediainfo0/0.7.61/libmediainfo0_0.7.61-1_amd64.Debian_6.0.deb
+if [ $? != 0 ]; then
+exit 34
+fi
 wget http://mediaarea.net/download/binary/mediainfo/0.7.61/mediainfo_0.7.61-1_amd64.Debian_6.0.deb
-
+if [ $? != 0 ]; then
+exit 35
+fi
 # On installe !
 dpkg -i *.deb
-
+if [ $? != 0 ]; then
+exit 36
+fi
 # On clean !
 cd && rm -Rf mediainfo
 
@@ -332,6 +438,9 @@ echo "#########################
 #On empêche la connexion root, il faudra se connecter avec un
 #utilisateur normal et obtenir les privilèges par la suite. (su / sudo)
 sed "s/PermitRootLogin/#PermitRootLogin/" /etc/ssh/sshd_config > ssh.config
+if [ $? != 0 ]; then
+exit 37
+fi
 rm /etc/ssh/sshd_config
 mv ssh.config /etc/ssh/sshd_config
 
@@ -344,21 +453,41 @@ echo "###############################
 ###############################"
 
 wget https://raw.github.com/synoga/DebianSeedboxInstaller/master/rtorrentd -O /etc/init.d/rtorrent
+if [ $? != 0 ]; then
+exit 38
+fi
 sed "s/XXXUSERXXX/$user/" /etc/init.d/rtorrent > /etc/init.d/rtorrentd
+if [ $? != 0 ]; then
+exit 39
+fi
 rm -f /etc/init.d/rtorrent
 
 chmod +x /etc/init.d/rtorrentd
 update-rc.d rtorrentd defaults
-
+if [ $? != 0 ]; then
+exit 40
+fi
 echo "###############################
 #                             #
 #    Configuration du zsh     #
 #                             #
 ###############################"
 wget http://formation-debian.via.ecp.fr/fichiers-config/zshrc
+if [ $? != 0 ]; then
+exit 41
+fi
 wget http://formation-debian.via.ecp.fr/fichiers-config/zshenv
+if [ $? != 0 ]; then
+exit 42
+fi
 wget http://formation-debian.via.ecp.fr/fichiers-config/zlogin
+if [ $? != 0 ]; then
+exit 43
+fi
 wget http://formation-debian.via.ecp.fr/fichiers-config/zlogout
+if [ $? != 0 ]; then
+exit 44
+fi
 wget http://formation-debian.via.ecp.fr/fichiers-config/dir_colors
 mv zshrc zshenv zlogin zlogout dir_colors /etc/zsh
 
@@ -374,7 +503,13 @@ cd
 mkdir owncloud
 cd owncloud
 wget http://download.owncloud.org/community/owncloud-5.0.10.tar.bz2
+if [ $? != 0 ]; then
+exit 45
+fi
 wget http://download.owncloud.org/community/owncloud-5.0.10.tar.bz2.md5
+if [ $? != 0 ]; then
+exit 46
+fi
 md5sum -c --status owncloud-5.0.10.tar.bz2.md5 < owncloud-5.0.10.tar.bz2
 if [ $? = 0 ]; then
     tar -xjf owncloud-5.0.10.tar.bz2
@@ -392,7 +527,9 @@ echo "#################################
 #################################"
 
 python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main(install_dir='/opt')"
-
+if [ $? != 0 ]; then
+exit 47
+fi
 
 echo "################################
 #                              #
@@ -403,12 +540,21 @@ echo "################################
 
 #Démarrage du démon rtorrent
 service rtorrentd start
+if [ $? != 0 ]; then
+exit 48
+fi
 
 #Redémarrage d'Apache
 service apache2 start
+if [ $? != 0 ]; then
+exit 49
+fi
 
 #Puis on redémarre le démon ssh
 service ssh restart
+if [ $? != 0 ]; then
+exit 50
+fi
 
 clear
 echo "Pour accéder à votre serveur : http://$IP/
@@ -426,3 +572,6 @@ Le certificat de chiffrement étant autosigné, certains navigateurs vous offrir
 "
 echo "Terminé."
 reboot
+if [ $? != 0 ]; then
+exit 51
+fi
